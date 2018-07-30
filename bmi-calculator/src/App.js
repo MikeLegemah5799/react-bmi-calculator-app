93dd3a5be8ec5704f6382fcfca336363b2b64cc0
Michael Legemah <MichaelLegemah@gmail.com>
2018-07-29 22:52:23 -0400

    initial commit

---
+++ bmi-calculator/src/App.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+
+import './App.css';
+
+class App extends Component {
+
+constructor(props){
+  super(props);
+  this.state = {
+    height:170,
+    weight: 65,
+    bmi: 22.49,
+    bmiClass: 'Normal'
+  }
+}
+
+  render() {
+    return (
+      <div className="App">
+        <h1>BMI Calculator</h1>
+        <form></form>
+      </div>
+    );
+  }
+}
+
+export default App;
